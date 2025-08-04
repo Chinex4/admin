fc3caf941e6796d43c45446e8178d4a420863d51
Chinex <astauchiha234@gmail.com>
2025-08-04 17:29:42 +0100

    new

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,11 +2,14 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //   baseURL: 'http://172.20.10.2/cashtradeproApi/api/task/',
-    baseURL: 'http://192.168.1.238/cashtradeproApi/api/task/',
-  // baseURL: 'http://192.168.211.134/backend/api/task/',
-  // baseURL: "https://api.cashtradepro.com/api/task/",
-  // withCredentials: true,
+  // baseURL: 'http://172.20.10.2/cashtradeproApi/api/task/',
+	// baseURL: 'http://172.19.18.22/backend/api/task/',
+	// baseURL: 'http://192.168.1.238/cashtradeproApi/api/task/',
+	// baseURL: 'http://10.84.244.22/backend/api/task/',
+	// baseURL: 'http://192.168.1.117/backend/api/task/',
+	// baseURL: 'http://192.168.103.22/backend/api/task/',
+	baseURL: 'http://192.168.1.212/backend/api/task/',
+	// baseURL: 'https://api.cashtradepro.com/api/task/',
 });
 
 // Attach token if exists
