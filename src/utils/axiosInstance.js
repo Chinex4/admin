19ad93797a4b687c901d07347ae6bf2f1ace7560
EHenryRichard <rickemzy5@gmail.com>
2026-02-18 21:53:05 +0100

    fr

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,12 +1,12 @@
 // src/utils/axiosInstance.js
 import axios from 'axios';
 
-// const API_BASE_URL =
-//   import.meta.env.VITE_API_BASE_URL ||
-//   'https://api.bitspotexchange.com/api/task/';
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  'http://127.0.0.1/cashtradeproApi/api/task/';
+  'https://api.bitspotexchange.com/api/task/';
+// const API_BASE_URL =
+//   import.meta.env.VITE_API_BASE_URL ||
+//   'http://127.0.0.1/cashtradeproApi/api/task/';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
